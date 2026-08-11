4123236284b47f838326fb0fa2b8eb69a26f3bc1
Keffin <kevin.alemi97@gmail.com>
2026-08-11 14:52:21 +0200

    feat: adding versions view and refactoring code base

--- src/tui/Search/hooks/useSearchNavigation.ts
+++ src/tui/Search/hooks/useSearchNavigation.ts
@@ -1,38 +1,63 @@
-import { useEffect, useState } from "react";
-import { SearchPackagesResult } from "../../types/types.js";
+import { useEffect, useMemo, useState } from "react";
+import { MAX_VERSIONS, Mode, SearchPackagesResult } from "../../types/types.js";
 import { useInput } from "ink";
 import { FORMATS } from "../utils.js";
 import clipboard from "clipboardy";
 
 export const useSearchNavigation = (results: SearchPackagesResult) => {
+  // navigation
   const [selectedIndex, setSelectedIndex] = useState<number>(0);
+  const [mode, setMode] = useState<Mode>("packages");
+  const [selectedVersionIndex, setSelectedVersionIndex] = useState<number>(0);
+
+  const pkg = results[selectedIndex];
+  const reversedVersions = useMemo(
+    () => [...(pkg?.versions ?? [])].reverse().slice(0, MAX_VERSIONS),
+    [pkg],
+  );
+
+  // UI feedback
   const [copied, setCopied] = useState<boolean>(false);
   const [formatIndex, setFormatIndex] = useState<number>(0);
 
+
   useEffect(() => {
     setSelectedIndex(0);
+    setMode("packages");
   }, [results]);
 
   useInput((_, key) => {
-    if (key.return && results[selectedIndex]) {
-      const pkg = results[selectedIndex];
+    if (key.return && pkg) {
+      const version =
+        mode === "versions"
+          ? (reversedVersions[selectedVersionIndex]?.version ?? pkg.version)
+          : pkg.version;
+
       clipboard.writeSync(
-        FORMATS[formatIndex]!.render(pkg.id ?? "", pkg.version ?? ""),
+        FORMATS[formatIndex]!.render(pkg.id ?? "", version ?? ""),
       );
+
       setCopied(true);
       setTimeout(() => setCopied(false), 2000);
     }
 
+    if (key.rightArrow && pkg && mode === "packages") {
+      if (pkg.versions && pkg.versions.length > 0) {
+        setSelectedVersionIndex(0);
+        setMode("versions");
+      }
+    }
+
+    if (key.leftArrow && mode === "versions") {
+      setMode("packages");
+    }
+
     if (key.upArrow) {
-      setSelectedIndex((i) =>
-        i === 0 ? results.length - 1 : Math.max(0, i - 1),
-      );
+      navigateUp();
     }
 
     if (key.downArrow) {
-      setSelectedIndex((i) =>
-        i === results.length - 1 ? 0 : Math.min(results.length - 1, i + 1),
-      );
+      navigateDown();
     }
 
     if (key.tab) {
@@ -40,9 +65,31 @@ export const useSearchNavigation = (results: SearchPackagesResult) => {
     }
   });
 
+  const navigateUp = () => {
+    if (mode === "versions") {
+      setSelectedVersionIndex((i) =>
+        i === 0 ? reversedVersions.length - 1 : i - 1,
+      );
+    } else {
+      setSelectedIndex((i) => (i === 0 ? results.length - 1 : i - 1));
+    }
+  };
+
+  const navigateDown = () => {
+    if (mode === "versions") {
+      setSelectedVersionIndex((i) =>
+        i === reversedVersions.length - 1 ? 0 : i + 1,
+      );
+    } else {
+      setSelectedIndex((i) => (i === results.length - 1 ? 0 : i + 1));
+    }
+  };
+
   const currentFormat = FORMATS[formatIndex]!;
   return {
     selectedIndex,
+    selectedVersionIndex,
+    mode,
     copied,
     currentFormat,
   };
--- src/tui/types/types.ts
+++ src/tui/types/types.ts
@@ -1,3 +1,11 @@
 import { searchPackages } from "../../nuget-client/nuget-client.js";
 
 export type SearchPackagesResult = Awaited<ReturnType<typeof searchPackages>>;
+export type Package = NonNullable<SearchPackagesResult[number]>;
+
+export type Mode = "packages" | "versions";
+
+// +4 for "> " and padding
+export const PADDING = 4;
+
+export const MAX_VERSIONS = 10;
